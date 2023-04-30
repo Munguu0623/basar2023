a5cb05519da1609d4fd63cbf22670f321a0bc43b
nouram <dashdorjuuganbayr@gmail.com>
2023-04-30 15:04:50 +0800

    commit

--- src/pages/news/[slug].js
+++ src/pages/news/[slug].js
@@ -2,15 +2,23 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import Navbar from "../../../components/Layout/Navbar";
+import dayjs from "dayjs";
 
 export default function News({ post }) {
   const router = useRouter();
 
   return (
     <Navbar>
-      <div className="max-w-5xl mx-auto">
+      <div className="max-w-4xl mx-auto">
+        <div className="text-[#2F327D] md:text-3xl text-2xl font-bold text-center my-10">
+          {post.attributes.title}
+        </div>
+        <div className="flex justify-end mb-3">
+          {dayjs(post.attributes.publishedAt).format("YYYY-MM-DD")}
+        </div>
         <ReactMarkdown
-          children={post.attributes.data.replace(
+          className="mb-16"
+          children={post.attributes.data.replaceAll(
             "](/",
             "](http://103.168.56.133:1337/"
           )}
